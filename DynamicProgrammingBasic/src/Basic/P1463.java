935843712a2f9ccb3e5ad3cd8b46656f8e855592
kms <k_rush@naver.com>
2020-07-10 10:42:53 +0900

    Problem 11726 Complete

--- DynamicProgrammingBasic/src/Basic/P1463.java
+++ DynamicProgrammingBasic/src/Basic/P1463.java
@@ -9,7 +9,7 @@ package Basic;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-class Number {
+class Number { // 수
 	int N;
 	int result;
 	
@@ -22,26 +22,27 @@ class Number {
 public class P1463 {
 
 	public static void main(String[] args) {
+		//input 
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
 		
-		if (num < 1 || num > 1000000) {
-			return ;// TODO Auto-generated method stub
+		if (num < 1 || num > 1000000) { // 범위에 맞지 않으면 프로그램 종료
+			return ;
 		}
 
-		Number N = new Number(num, 0);
+		Number N = new Number(num, 0); // 값을 Number 클래스를 이용해 초기화
 		
-		int result = Integer.MAX_VALUE;
-		LinkedList<Number> queue = new LinkedList<>();
-		queue.add(N);
+		int result = Integer.MAX_VALUE; // 결과 값
+		LinkedList<Number> queue = new LinkedList<>(); // BFS를 위한 QUEUE
+		queue.add(N); // 첫 수 삽입 
 		
-		while (!queue.isEmpty()) {
-			Number Start = queue.pollFirst();
-			if (Start.N == 1) {
+		while (!queue.isEmpty()) { // 큐가 빌 때까지 반복
+			Number Start = queue.pollFirst(); // 값을 가져옴
+			if (Start.N == 1) { // 값이 1일 때
 				result = Start.result;
 				break;
 			}
-			if (Start.N % 3 == 0) {
+			if (Start.N % 3 == 0) { // 값이 3으로 나누어 떨어질 때
 				Number temp = new Number(Start.N / 3, Start.result + 1);
 				if (temp.N == 1) {
 					result = temp.result;
@@ -50,7 +51,7 @@ public class P1463 {
 					queue.add(temp);
 				}
 			}
-			if (Start.N % 2 == 0) {
+			if (Start.N % 2 == 0) { // 값이 2로 나누어 떨어질 때
 				Number temp = new Number(Start.N / 2, Start.result + 1);
 				if (temp.N == 1) {
 					result = temp.result;
@@ -59,6 +60,7 @@ public class P1463 {
 					queue.add(temp);
 				}
 			}
+			// 값에서 1을 뺄 때
 			Number temp = new Number(Start.N - 1, Start.result + 1);
 			if (temp.N == 1) {
 				result = temp.result;
@@ -67,7 +69,7 @@ public class P1463 {
 				queue.add(temp);
 			}
 		}
+		//output
 		System.out.println(result);
 	}
-
 }
